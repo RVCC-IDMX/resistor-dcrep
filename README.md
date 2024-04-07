1ea2026c3bff96af36cf6860a7ea3859df1f6a55
Daniel C <63077752+dcrep@users.noreply.github.com>
2024-04-06 23:04:27 -0400

    working resistor calculator

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Resistor
+This project serves as an interactive Resistor Calculator
+
+Run index.html, and click a color on each section on the left
+to update the resistor and values on the right
--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 import { getResistorOhms } from './resistor.js';
+
+let bands = {
+    color1: 'red',
+    color2: 'green',
+    multiplier: 'violet',
+    tolerance: 'gold'
+};
+
+// Element for 'Resistor value' string
+const textOutput = document.getElementsByClassName('answer')[0];
+// Resistor container
+const resistor = document.getElementsByClassName('resistor')[0];
+//console.log(resistor);
+
+// All color containers
+const containers = document.querySelectorAll('.container');
+// Add event listener to each container
+containers.forEach(item => {
+    item.addEventListener('click', e => {
+      //console.log('Clicked container.')
+
+      // Get all classes (should be 'box' and color)
+      const classes = e.target.classList;
+
+      if (classes.contains('box') && classes.length == 2)
+      {
+        const color = classes.item(1);
+        const parent = e.target.parentNode;
+        // 'color#' -> pull out the #:
+        const bandID = parent.id.slice(5)
+        // Get the element with id 'b#'
+        const ceramicBand = resistor.querySelector('#b' + bandID);
+        // Previou color
+        const bandColor = ceramicBand.classList.item(1);
+        // Remove previous color
+        ceramicBand.classList.remove(bandColor);
+        // Add current color
+        ceramicBand.classList.add(color);
+
+        // Set the appropriate field in the color object
+        switch(+bandID) {
+            case 0:
+                //console.log('Band 1');
+                bands.color1 = color;                
+                break;
+            case 1:
+                //console.log('Band 2');
+                bands.color2 = color;
+                break;
+            case 2:
+                //console.log('Multiplier');
+                bands.multiplier = color;
+                break;
+            case 3:
+                //console.log('Tolerance');
+                bands.tolerance = color;
+                break;
+        }
+        // Display the calculated value
+        textOutput.textContent = getResistorOhms(bands);
+      }
+    });
+  });
